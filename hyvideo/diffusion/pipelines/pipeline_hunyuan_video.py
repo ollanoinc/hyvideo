cd90e767488ac4d1ec57721a4a58ae93fe2fc964
Gregory D. Hunkins <greg@ollano.com>
2024-12-09 16:00:30 -0500

    simplify vae tiling

--- hyvideo/diffusion/pipelines/pipeline_hunyuan_video.py
+++ hyvideo/diffusion/pipelines/pipeline_hunyuan_video.py
@@ -234,6 +234,14 @@ class HunyuanVideoPipeline(DiffusionPipeline):
         self.vae_scale_factor = 2 ** (len(self.vae.config.block_out_channels) - 1)
         self.image_processor = VaeImageProcessor(vae_scale_factor=self.vae_scale_factor)
 
+    def enable_vae_tiling(self):
+        r"""
+        Enable tiled VAE decoding. When this option is enabled, the VAE will split the input tensor into tiles to
+        compute decoding and encoding in several steps. This is useful for saving a large amount of memory and to allow
+        processing larger videos.
+        """
+        self.vae.enable_tiling()
+
     def encode_prompt(
         self,
         prompt,
@@ -447,16 +455,12 @@ class HunyuanVideoPipeline(DiffusionPipeline):
             negative_attention_mask,
         )
 
-    def decode_latents(self, latents, enable_tiling=True):
+    def decode_latents(self, latents):
         deprecation_message = "The decode_latents method is deprecated and will be removed in 1.0.0. Please use VaeImageProcessor.postprocess(...) instead"
         deprecate("decode_latents", "1.0.0", deprecation_message, standard_warn=False)
 
         latents = 1 / self.vae.config.scaling_factor * latents
-        if enable_tiling:
-            self.vae.enable_tiling()
-            image = self.vae.decode(latents, return_dict=False)[0]
-        else:
-            image = self.vae.decode(latents, return_dict=False)[0]
+        image = self.vae.decode(latents, return_dict=False)[0]
         image = (image / 2 + 0.5).clamp(0, 1)
         # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
         if image.ndim == 4:
@@ -694,7 +698,6 @@ class HunyuanVideoPipeline(DiffusionPipeline):
         callback_on_step_end_tensor_inputs: List[str] = ["latents"],
         freqs_cis: Tuple[torch.Tensor, torch.Tensor] = None,
         vae_ver: str = "88-4c-sd",
-        enable_tiling: bool = False,
         n_tokens: Optional[int] = None,
         embedded_guidance_scale: Optional[float] = None,
         **kwargs,
@@ -1068,15 +1071,9 @@ class HunyuanVideoPipeline(DiffusionPipeline):
             with torch.autocast(
                 device_type="cuda", dtype=vae_dtype, enabled=vae_autocast_enabled
             ):
-                if enable_tiling:
-                    self.vae.enable_tiling()
-                    image = self.vae.decode(
-                        latents, return_dict=False, generator=generator
-                    )[0]
-                else:
-                    image = self.vae.decode(
-                        latents, return_dict=False, generator=generator
-                    )[0]
+                image = self.vae.decode(
+                    latents, return_dict=False, generator=generator
+                )[0]
 
             if expand_temporal_dim or image.shape[2] == 1:
                 image = image.squeeze(2)
